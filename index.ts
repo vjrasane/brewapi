0a824e8ab9588761703c89418d0d84e6e13c074a
Ville Räsänen <git@vjm.anonaddy.me>
2025-05-19 16:40:22 +0300

    initial

---
+++ index.ts
@@ -0,0 +1,28 @@
+import { Elysia, status, t } from "elysia";
+
+const { API_KEY } = process.env;
+
+const app = new Elysia()
+
+app.onBeforeHandle(async ({ query }) => {
+  const { apiKey } = query;
+  if (apiKey !== API_KEY) {
+    return status(401);
+  }
+})
+
+app.all("/api/v1/data", ({ body, headers }) => {
+  console.log("----- REQUEST -----");
+  console.log(new Date().toISOString());
+  console.log("----- HEADERS -----");
+  console.log(headers);
+  console.log("----- BODY -----");
+  console.log(body)
+  console.log("----- END -----");
+})
+
+app.listen(3000);
+
+console.log(
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+);
